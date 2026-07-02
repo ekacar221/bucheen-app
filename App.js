bd3f210f6c1356563905c0036f9ddb756f7095f5
Büşra K. <ekacar221@hotmail.com>
2026-07-02 21:05:45 +0300

    update

--- App.js
+++ App.js
@@ -3,10 +3,8 @@ import Navbar from "./src/components/Navbar";
 
 export default function App() {
   return (
-    // Tüm sayfa dikey kaydırılabilir
     <ScrollView className="flex-1 bg-rose-50">
       <Navbar />
-      {/* Diğer komponentler buraya gelecek */}
     </ScrollView>
   );
 }
